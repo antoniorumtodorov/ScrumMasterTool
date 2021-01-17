2d08377091d3899c88908d67696c86f1ce03a699
Antonio <antoniorumtodorov@gmail.com>
2021-01-17 10:19:40 +0200

    Add Convert C# object to Json
    Tool can be use for convert obejcts in json which make more easy way for testing data in the future

---
+++ ObjectConverterJson/ConvertObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectConverterJson
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+    class ConvertObject
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new List<Member>()
+            {
+                new Member{Name = "Pesho", Email="pesho@gmail.com"},
+                new Member{Name="Antonio", Email="antonio@gmail.com"}
+            };
+
+            string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(list);
+            Console.WriteLine(jsonString);
+        }
+    }
+}
